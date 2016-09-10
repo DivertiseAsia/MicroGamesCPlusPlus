1634fec295254e09782f674ce4bdffcad89576c8
Hunter Williams <hunter@huntergwilliams.com>
2016-09-10 12:13:53 +0700

    Added basic splash screen

---
+++ Classes/SplashScene.cpp
@@ -0,0 +1,53 @@
+#include "SplashScene.h"
+#include "SimpleAudioEngine.h"
+#include "HelloWorldScene.h"
+
+USING_NS_CC;
+
+Scene* Splash::createScene()
+{
+    // 'scene' is an autorelease object
+    auto scene = Scene::create();
+    
+    // 'layer' is an autorelease object
+    auto layer = Splash::create();
+
+    // add layer as a child to scene
+    scene->addChild(layer);
+
+    // return the scene
+    return scene;
+}
+
+// on "init" you need to initialize your instance
+bool Splash::init()
+{
+    //////////////////////////////
+    // 1. super init first
+    if ( !Layer::init() )
+    {
+        return false;
+    }
+    
+    auto visibleSize = Director::getInstance()->getVisibleSize();
+    Vec2 origin = Director::getInstance()->getVisibleOrigin();
+
+    // add "HelloWorld" splash screen"
+    auto sprite = Sprite::create("splash.png");
+
+    // position the sprite on the center of the screen
+    sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
+
+    // add the sprite as a child to this layer
+    this->addChild(sprite, 0);
+
+	this->scheduleOnce(schedule_selector(Splash::endSplash), 1.0f);
+    
+    return true;
+}
+
+
+void Splash::endSplash(float duration)
+{
+	Director::getInstance()->replaceScene(HelloWorld::createScene());  
+}
